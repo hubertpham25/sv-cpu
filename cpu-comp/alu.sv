dd10adf48358e1d7b8dc022e49b104c92c725a3c
hubertpham25 <phamhubert820@gmail.com>
2026-08-08 02:06:41 -0400

    started basic instructions. need to solve default case

--- cpu-comp/alu.sv
+++ cpu-comp/alu.sv
@@ -1,3 +1,21 @@
-module ALU();
+typedef enum {ADD, SUB, OP_AND, OP_OR, OP_XOR} opcode;
 
-endmodule;
+module ALU(
+    input logic[31:0] input_a,
+    input logic[31:0] input_b,
+    input opcode opcode_t,
+    output logic [31:0] result,
+    output logic zero
+);
+    always_comb begin
+        case (opcode_t)
+            ADD: result = input_a + input_b;
+            SUB: result = input_a - input_b;
+            OP_AND: result = input_a & input_b;
+            OP_OR: result = input_a | input_b;
+            OP_XOR: result = (input_a | input_b) & ~(input_a & input_b);
+            //default:
+            //    result = zero
+        endcase
+    end
+endmodule
