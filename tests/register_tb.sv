bff68959f50f93506654ecf4966ce011856678d8
hubertpham25 <phamhubert820@gmail.com>
2026-08-13 21:06:46 -0400

    all tests work

--- tests/register_tb.sv
+++ tests/register_tb.sv
@@ -15,6 +15,39 @@ module registerf_tb;
         .out_r1(out_r1), .out_r2(out_r2)
     );
 
-    
+    initial begin // clock/tick generator
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
 
+    initial begin // actual testing and setting vars. dependent on the clk
+        wr_enable = 1; // write enable on
+        r1 = 1; // register 1
+        r2 = 2; // register 2
+        w1 = 1; // write to register 1
+        wr_data = 10; // write 10
+        @(posedge clk);
+        @(posedge clk);
+        $display("register %d: %d (expected 10)", r1, out_r1); #5;
+        $display("register %d: %d (expected 0)", r2, out_r2); #5;
+        // test 2: with write to special register
+        wr_enable = 1;
+        r1 = 0;
+        r2 = 1;
+        w1 = 0;
+        wr_data = 10;
+        @(posedge clk);
+        @(posedge clk);
+        $display("register %d: %d (writing to special register -> 0)", r1, out_r1); #5;
+        $display("register %d: %d (expected 10)", r2, out_r2); #5
+        // test 3: reading
+        wr_enable = 0; 	
+        r1 = 31;
+        r2 = 1;
+        @(posedge clk);
+        @(posedge clk);
+        $display("register %d: %d (expected 0)", r1, out_r1); #5;
+        $display("register %d: %d (expected 10)", r2, out_r2); #5;
+        $finish;
+    end
 endmodule
