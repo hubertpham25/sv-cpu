ee40da6639c46400c1540db65dd4dff634f81907
hubertpham25 <phamhubert820@gmail.com>
2026-08-23 13:34:51 -0400

    created the i type

--- cpu-comp/control_unit.sv
+++ cpu-comp/control_unit.sv
@@ -2,10 +2,10 @@ import packages::*;
 module control_unit(
     input logic[31:0] instr,
     output logic reg_wr, // should it write to a register
-    output logic alu_src, // does ALU second operand come from a register or immediate
+    output logic alu_src, // does ALU second operand come from a register (1) or immediate (0)
     output logic mem_rd, // should data memory be read this cycle
     output logic mem_wr, // should data memory be written this cycle
-    output logic mem_to_reg, // when writing to a register, does value come from ALU result or from data memory
+    output logic mem_to_reg, // when writing to register, does value come from ALU result (0) or data memory (1)
     output logic branch, // is it a branch instruction
     output packages::opcode_t alu_op
 );
@@ -45,6 +45,27 @@ module control_unit(
                     alu_op = packages::OP_SLT;
                 end
             end
+            packages::ADDI_OP: begin //I-type. Might not be worth renaming and touching other files
+                funct3 = instr[14:12];
+                alu_src = 0;
+                mem_rd = 0;
+                branch = 0;
+                mem_to_reg = 0
+                mem_wr = 1;
+                reg_wr = 1;
+                if (funct3 == 3'h0) begin //addi
+                    alu_op = packages::ADD;
+                end
+                else if (funct3 = 3'h4) begin //xori
+                    alu_op = packages::OP_XOR;
+                end
+                else if (funct3 = 3'h6) begin //ori
+                    alu_op = packages::OP_OR;
+                end
+                else if (funct3 = 3'h4) begin //andi
+                    alu_op = packages::OP_AND;
+                end
+            end 
         endcase
     end
 endmodule
