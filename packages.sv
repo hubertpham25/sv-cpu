5c01ed240c4108e1d8f3f5c01c459c72a04dbce2
hubertpham25 <phamhubert820@gmail.com>
2026-08-23 16:08:30 -0400

    added lw

--- packages.sv
+++ packages.sv
@@ -5,6 +5,7 @@ package packages;
         opcode_t opcode;
     } instructions_t;
 
-    typedef enum logic[6:0] {ADDI_OP = 7'b0010011, 
-            SW_OP = 7'b0100011, BEQ_OP = 7'b1100011, RTYPE = 7'b0110011} immed_opcode_t;
+    typedef enum logic[6:0] {ADDI_OP = 7'b0010011, SW_OP = 7'b0100011, 
+                            BEQ_OP = 7'b1100011, RTYPE = 7'b0110011,
+                            LW_OP = 7'b0000011} immed_opcode_t;
 endpackage
