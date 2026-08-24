d3f5a38c3aec7b0d3b6054c841e3814664eb01a0
hubertpham25 <phamhubert820@gmail.com>
2026-08-23 23:03:25 -0400

    temporarily initialized some instructions so there can be something to read

--- cpu-comp/instr_mem.sv
+++ cpu-comp/instr_mem.sv
@@ -6,4 +6,10 @@ module instr_mem(
     logic[5:0] instr_addr;
     assign instr_addr = PC[7:2];
     assign instr_word = instr_memory[instr_addr];
+    //temp inits
+    initial begin
+        instr_memory[0] = {7'b0000000, 5'd2, 5'd1, 3'b000, 5'd3, 7'b0110011};
+        instr_memory[1] = {7'b0100000, 5'd2, 5'd1, 3'b000, 5'd4, 7'b0110011};
+        instr_memory[2] = {12'd10, 5'd1, 3'b000, 5'd5, 7'b0010011};
+    end
 endmodule
