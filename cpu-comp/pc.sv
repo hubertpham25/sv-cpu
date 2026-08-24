9ec5b93bf238b7adebd4e483ef92d599363f9534
hubertpham25 <phamhubert820@gmail.com>
2026-08-24 17:55:22 -0400

    pc changes on positive clk

--- cpu-comp/pc.sv
+++ cpu-comp/pc.sv
@@ -1,4 +1,9 @@
 module pc(
-    input logic clk;
+    input logic clk,
+    input logic[31:0] next_pc,
+    output logic[31:0] curr_pc
 );
+    always_ff @(posedge clk) begin: move_instr
+        curr_pc <= next_pc;
+    end: move_instr
 endmodule
