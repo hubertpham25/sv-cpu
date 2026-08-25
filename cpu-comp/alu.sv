a6ed9265f83855b705393d74efb967c9290afaa0
hubertpham25 <phamhubert820@gmail.com>
2026-08-24 20:59:15 -0400

    removed instructions_t. the struct wouldve created issues at top level integration

--- cpu-comp/alu.sv
+++ cpu-comp/alu.sv
@@ -1,17 +1,19 @@
 import packages::*;
 module ALU(
-    input packages::instructions_t instr,
+    input logic[31:0] a,
+    input logic[31:0] b,
+    input packages::opcode_t opcode
     output logic [31:0] result,
     output logic zero_fg
 );
     always_comb begin
-        case (instr.opcode)
-            packages::ADD: result = instr.a + instr.b;
-            packages::SUB: result = instr.a - instr.b;
-            packages::OP_AND: result = instr.a & instr.b;
-            packages::OP_OR: result = instr.a | instr.b;
-            packages::OP_XOR: result = (instr.a | instr.b) & ~(instr.a & instr.b);
-            packages::OP_SLT: result = (instr.a < instr.b) ? 1:0;
+        case (opcode)
+            packages::ADD: result = a + b;
+            packages::SUB: result = a - b;
+            packages::OP_AND: result = a & b;
+            packages::OP_OR: result = a | b;
+            packages::OP_XOR: result = (a | b) & ~(a & b);
+            packages::OP_SLT: result = (a < b) ? 1:0;
             default: result = 0;
         endcase
     end
