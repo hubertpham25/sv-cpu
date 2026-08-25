9cd292cffd14c4370fbd8a2d35b72e55ce2c53cb
hubertpham25 <phamhubert820@gmail.com>
2026-08-24 23:25:15 -0400

    created cu and reg file instances

---
+++ top_level_CPU.sv
@@ -0,0 +1,58 @@
+import packages::*;
+module CPU;
+    logic clk;
+
+    // output control unit wires
+    logic[31:0] instr; 
+    // ^ from instr_memory to control unit, shared between r1,r2 and w1 in register file
+    logic reg_wr;
+    logic alu_src;
+    logic mem_rd;
+    logic mem_wr;
+    logic mem_to_reg;
+    logic branch;
+    opcode_t alu_op;
+
+    // register file output wires
+    logic[31:0] out_r1, out_r2;
+
+    // ALU output wires
+    logic[31:0] result;
+    logic zero_fg; // goes to PC Adder
+
+    // Data memory output wires
+    logic[31:0] result;
+
+    // Immediate generator output wire
+    logic[31:0] imm_val;
+
+    // PC_Adder output wire
+    logic[31:0] next_pc;
+
+    // PC output wire
+    logic[31:0] curr_pc;
+
+    control_unit cu_inst(
+        .instr(instr),
+        .reg_wr(reg_wr),
+        .alu_src(alu_src),
+        .mem_rd(mem_rd),
+        .mem_wr(mem_wr),
+        .mem_to_reg(mem_to_reg),
+        .branch(branch),
+        .alu_op(alu_op)
+    );
+
+    reg_file regfile_inst(
+        .clk(clk),
+        .wr_data(reg_wr),
+        .r1(instr),
+        .r2(instr),
+        .w1(instr),
+        // make the mux behavior
+        .out_r1(out_r1),
+        .out_r2(out_r2)
+    );
+
+
+endmodule;
