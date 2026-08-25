7ffb71fa667a549e48a90b1d82a1eb657f1142f9
hubertpham25 <phamhubert820@gmail.com>
2026-08-25 14:53:13 -0400

    changed zero_flag to zero_fg

--- cpu-comp/pc_adder.sv
+++ cpu-comp/pc_adder.sv
@@ -2,7 +2,7 @@ module PC_ADDER(
     input logic[31:0] curr_pc,
     input logic[31:0] imm,
     input logic branch,
-    input logic zero_flag,
+    input logic zero_fg,
     output logic[31:0] next_pc
 );
     logic[31:0] branch_pc;
@@ -10,7 +10,7 @@ module PC_ADDER(
     assign branch_pc = curr_pc + imm;
     assign add_pc = curr_pc + 4;
     always_comb begin
-        if (branch == 1 && zero_flag == 1) begin
+        if (branch == 1 && zero_fg == 1) begin
             next_pc = branch_pc;
         end
         else begin
