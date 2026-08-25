9d59d87e13f13292f24058b7b7427681609eaeb7
hubertpham25 <phamhubert820@gmail.com>
2026-08-25 15:18:39 -0400

    uninit the temp memory and replaced with a program given

--- cpu-comp/instr_mem.sv
+++ cpu-comp/instr_mem.sv
@@ -6,10 +6,6 @@ module INSTR_MEM(
     logic[5:0] instr_addr;
     assign instr_addr = pc[7:2];
     assign instr_word = instr_memory[instr_addr];
-    //temp inits
-    initial begin
-        instr_memory[0] = {7'b0000000, 5'd2, 5'd1, 3'b000, 5'd3, 7'b0110011};
-        instr_memory[1] = {7'b0100000, 5'd2, 5'd1, 3'b000, 5'd4, 7'b0110011};
-        instr_memory[2] = {12'd10, 5'd1, 3'b000, 5'd5, 7'b0010011};
-    end
+    
+    $readmemh("program.hex", instr_memory);
 endmodule
