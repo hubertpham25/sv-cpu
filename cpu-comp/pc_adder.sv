bb958e858c4c3b5c9f873238f1e9d0e8506c90c6
hubertpham25 <phamhubert820@gmail.com>
2026-08-24 20:32:56 -0400

    init ports

---
+++ cpu-comp/pc_adder.sv
@@ -0,0 +1,9 @@
+module pc_adder(
+    input logic[31:0] curr_pc,
+    input logic[31:0] imm,
+    input logic branch,
+    input logic zero_flag,
+    output logic[31:0] next_pc
+);
+    
+endmodule
